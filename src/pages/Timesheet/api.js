b2d10bf871dab2ab1fb1330db38fd6a2a3d4517b
Ajaybhukya <ajaykumarbhukya511@gmail.com>
2025-08-11 10:01:57 +0530

    manager view

--- src/pages/Timesheet/api.js
+++ src/pages/Timesheet/api.js
@@ -3,7 +3,7 @@ import { showStatusToast } from "../../components/toastfy/toast";
 
 const apiEndpoint = "http://localhost:8080";
 
-export const fetchProjectTaskInfo = async ( userId) => {
+export const fetchProjectTaskInfo = async (userId) => {
   try {
     const response = await fetch(
       `${apiEndpoint}/api/timesheet/project-info/projects/${userId}`,
@@ -16,9 +16,9 @@ export const fetchProjectTaskInfo = async ( userId) => {
       }
     );
 
-http: if (!response.ok) {
-  throw new Error(`Error ${response.status}: ${response.statusText}`);
-}
+    http: if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
 
     const data = await response.json();
     return data;
@@ -32,27 +32,65 @@ http: if (!response.ok) {
   }
 };
 
+export const reviewTimesheet = async (
+  managerId,
+  timesheetId,
+  comment,
+  status
+) => {
+  try {
+    const res = await fetch(
+      `${apiEndpoint}/api/timesheets/review/${managerId}?status=${encodeURIComponent(
+        status
+      )}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({
+          timesheetId,
+          comment: comment,
+        }),
+      }
+    );
 
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData.message || "Failed to review timesheet");
+    }
+
+    showStatusToast(`Timesheet ${status} successfully`, "success");
+    return;
+  } catch (err) {
+    showStatusToast("Update failed", "error");
+    throw err;
+  }
+};
 
 export async function updateTimesheet(timesheetId, payload) {
   try {
-    const res = await fetch(`${apiEndpoint}/api/timesheet/update/${timesheetId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
+    const res = await fetch(
+      `${apiEndpoint}/api/timesheet/update/${timesheetId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      }
+    );
 
     if (!res.ok) {
-      const errorData = await res.json()
+      const errorData = await res.json();
       throw new Error(errorData.message || "Failed to update timesheet");
     }
 
     const data = res;
     showStatusToast("Timesheet updated successfully", "success");
     console.log("Update successful:", data);
-    
+
     return data;
   } catch (err) {
     showStatusToast(err.message || "Update failed", "error");
