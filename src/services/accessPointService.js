9e0d3d3365a5566a90018885d2657239c631da11
rohitlingarker <rohitlingerker@gmail.com>
2025-08-13 15:21:40 +0530

    Refactored backend api endpoint urls in services folder

--- src/services/accessPointService.js
+++ src/services/accessPointService.js
@@ -1,17 +1,17 @@
 // accessPointService.js
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: `${import.meta.env.VITE_USER_MANAGEMENT_URL}`,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // 🔐 Add token before every request
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token'); 
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -20,27 +20,34 @@ axiosInstance.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-const ACCESS_POINT_URL = '/admin/access-points/';
+const ACCESS_POINT_URL = "/admin/access-points/";
 
 const UNMAPPED_ACCESS_POINTS_URL = `${ACCESS_POINT_URL}unmapped-access-points`;
 
-export const listAccessPointsumapped = () => axiosInstance.get(UNMAPPED_ACCESS_POINTS_URL);
+export const listAccessPointsumapped = () =>
+  axiosInstance.get(UNMAPPED_ACCESS_POINTS_URL);
 export const listAccessPoints = () => axiosInstance.get(ACCESS_POINT_URL);
-export const getAccessPoint = (id) => axiosInstance.get(`${ACCESS_POINT_URL}${id}`);
-export const createAccessPoint = (data) => axiosInstance.post(ACCESS_POINT_URL, data);
-export const updateAccessPoint = (id, data) => axiosInstance.put(`${ACCESS_POINT_URL}${id}`, data);
-export const deleteAccessPoint = (id) => axiosInstance.delete(`${ACCESS_POINT_URL}${id}`);
+export const getAccessPoint = (id) =>
+  axiosInstance.get(`${ACCESS_POINT_URL}${id}`);
+export const createAccessPoint = (data) =>
+  axiosInstance.post(ACCESS_POINT_URL, data);
+export const updateAccessPoint = (id, data) =>
+  axiosInstance.put(`${ACCESS_POINT_URL}${id}`, data);
+export const deleteAccessPoint = (id) =>
+  axiosInstance.delete(`${ACCESS_POINT_URL}${id}`);
 
 // Module List API for dropdown
-export const listModules = () => axiosInstance.get(`${ACCESS_POINT_URL}modules`);
+export const listModules = () =>
+  axiosInstance.get(`${ACCESS_POINT_URL}modules`);
 
 // Get unmapped permissions (permissions not assigned to any access point)
-export const getUnmappedPermissions = () => axiosInstance.get('/admin/permissions/');
-
+export const getUnmappedPermissions = () =>
+  axiosInstance.get("/admin/permissions/");
 
 // Assign permission to access point
-export const assignPermissionToAccessPoint = (accessPointId, permissionId) => 
-  axiosInstance.post(`${ACCESS_POINT_URL}${accessPointId}/map-permission/${permissionId}`);
+export const assignPermissionToAccessPoint = (accessPointId, permissionId) =>
+  axiosInstance.post(
+    `${ACCESS_POINT_URL}${accessPointId}/map-permission/${permissionId}`
+  );
 
 // console.log(`${ACCESS_POINT_URL}${accessPointId}/map-permission/${permissionId}`);
-
--- src/services/roleManagementService.js
+++ src/services/roleManagementService.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Create axios instance with base configuration
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: `${import.meta.env.USER_MANAGEMENT_URL}`,
   timeout: 10000,
 });
 
 // Request interceptor to add auth token
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -18,27 +18,39 @@ axiosInstance.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-const ROLE_URL = '/admin/roles';
+const ROLE_URL = "/admin/roles";
 
 // Basic Role Management APIs
 export const listRoles = () => axiosInstance.get(ROLE_URL);
 export const getRole = (id) => axiosInstance.get(`${ROLE_URL}/${id}`);
 export const createRole = (roleData) => axiosInstance.post(ROLE_URL, roleData);
-export const updateRole = (id, roleData) => axiosInstance.put(`${ROLE_URL}/${id}`, roleData);
+export const updateRole = (id, roleData) =>
+  axiosInstance.put(`${ROLE_URL}/${id}`, roleData);
 export const deleteRole = (id) => axiosInstance.delete(`${ROLE_URL}/${id}`);
 
 // Permission Group Management APIs
-export const getPermissionsByRole = (roleId) => axiosInstance.get(`${ROLE_URL}/${roleId}/permissions`);
-export const getPermissionGroupsByRole = (roleId) => axiosInstance.get(`${ROLE_URL}/${roleId}/groups`);
-export const getAvailablePermissionGroupsForRole = (roleId) => axiosInstance.get(`${ROLE_URL}/${roleId}/available-groups`);
+export const getPermissionsByRole = (roleId) =>
+  axiosInstance.get(`${ROLE_URL}/${roleId}/permissions`);
+export const getPermissionGroupsByRole = (roleId) =>
+  axiosInstance.get(`${ROLE_URL}/${roleId}/groups`);
+export const getAvailablePermissionGroupsForRole = (roleId) =>
+  axiosInstance.get(`${ROLE_URL}/${roleId}/available-groups`);
 
-export const updatePermissionGroupsForRole = (roleId, groupIds) => axiosInstance.put(`${ROLE_URL}/${roleId}/groups`, { group_ids: groupIds });
-export const addPermissionGroupsToRole = (roleId, groupIds) => axiosInstance.post(`${ROLE_URL}/${roleId}/groups`, { group_ids: groupIds });
-export const removePermissionGroupFromRole = (roleId, groupId) => axiosInstance.delete(`${ROLE_URL}/${roleId}/groups/${groupId}`);
+export const updatePermissionGroupsForRole = (roleId, groupIds) =>
+  axiosInstance.put(`${ROLE_URL}/${roleId}/groups`, { group_ids: groupIds });
+export const addPermissionGroupsToRole = (roleId, groupIds) =>
+  axiosInstance.post(`${ROLE_URL}/${roleId}/groups`, { group_ids: groupIds });
+export const removePermissionGroupFromRole = (roleId, groupId) =>
+  axiosInstance.delete(`${ROLE_URL}/${roleId}/groups/${groupId}`);
 
 // Access Point Management APIs
-export const getAccessPointsByRole = (roleId) => axiosInstance.get(`${ROLE_URL}/${roleId}/access-points`);
-export const getAvailableAccessPointsForRole = (roleId) => axiosInstance.get(`${ROLE_URL}/${roleId}/access-points/available`);
-export const mapAccessPointToRole = (roleId, accessId) => axiosInstance.post(`${ROLE_URL}/${roleId}/access-points/${accessId}`);
-export const removeAccessPointFromRole = (roleId, accessId) => axiosInstance.delete(`${ROLE_URL}/${roleId}/access-points/${accessId}`);
-export const getAllAccessPoints = () => axiosInstance.get(`${ROLE_URL}/access-points/all`); 
+export const getAccessPointsByRole = (roleId) =>
+  axiosInstance.get(`${ROLE_URL}/${roleId}/access-points`);
+export const getAvailableAccessPointsForRole = (roleId) =>
+  axiosInstance.get(`${ROLE_URL}/${roleId}/access-points/available`);
+export const mapAccessPointToRole = (roleId, accessId) =>
+  axiosInstance.post(`${ROLE_URL}/${roleId}/access-points/${accessId}`);
+export const removeAccessPointFromRole = (roleId, accessId) =>
+  axiosInstance.delete(`${ROLE_URL}/${roleId}/access-points/${accessId}`);
+export const getAllAccessPoints = () =>
+  axiosInstance.get(`${ROLE_URL}/access-points/all`);
