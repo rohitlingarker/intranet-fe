01afffae1f60b6218d344bc3d3fef7eb0a476060
SricharanReddy129 <sricharanreddy129@gmail.com>
2025-10-01 10:32:37 +0530

    edit-role error dynamic handling

--- src/App.jsx
+++ src/App.jsx
@@ -82,18 +82,18 @@ const ProtectedRoute = ({ children, allowedRoles }) => {
     return <Navigate to="/" state={{ from: location.pathname }} replace />;
   }
 
-  if (allowedRoles && allowedRoles.length > 0) {
-    const hasRole = user?.roles?.some((role) => allowedRoles.includes(role));
-    console.log("ProtectedRoute check:", {
-      isAuthenticated,
-      user,
-      allowedRoles,
-      match: user?.roles?.some((role) => allowedRoles.includes(role)),
-    });
-    if (!hasRole) {
-      return <Navigate to="/unauthorized" replace />;
-    }
-  }
+  // if (allowedRoles && allowedRoles.length > 0) {
+  //   const hasRole = user?.roles?.some((role) => allowedRoles.includes(role));
+  //   console.log("ProtectedRoute check:", {
+  //     isAuthenticated,
+  //     user,
+  //     allowedRoles,
+  //     match: user?.roles?.some((role) => allowedRoles.includes(role)),
+  //   });
+  //   if (!hasRole) {
+  //     return <Navigate to="/unauthorized" replace />;
+  //   }
+  // }
   return <>{children}</>;
 };
 
@@ -210,7 +210,7 @@ const AppRoutes = () => {
 
           {/* User Management */}
 
-          {/* <Route path="/user-management/users" element={<UsersTable />} />
+          <Route path="/user-management/users" element={<UsersTable />} />
           <Route
             path="/user-management/users/create"
             element={<CreateUser />}
@@ -259,9 +259,9 @@ const AppRoutes = () => {
           <Route
             path="/user-management/access-points/admin/access-point-mapping"
             element={<AccessPointMapping />}
-          /> */}
+          />
 
-          <Route
+          {/* <Route
             path="/user-management/users"
             element={
               <ProtectedRoute allowedRoles={["Admin", "Super Admin"]}>
@@ -372,7 +372,7 @@ const AppRoutes = () => {
                 <AccessPointMapping />
               </ProtectedRoute>
             }
-          />
+          /> */}
 
           {/* Leave Management */}
           <Route
