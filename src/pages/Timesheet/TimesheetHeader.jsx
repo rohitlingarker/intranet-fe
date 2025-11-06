5adc29fac838afcf338642ccfa4c63def6cd7c99
Ajaybhukya <ajaykumarbhukya511@gmail.com>
2025-11-06 12:05:27 +0530

    Timesheet Header Fixes

--- src/pages/Timesheet/TimesheetHeader.jsx
+++ src/pages/Timesheet/TimesheetHeader.jsx
@@ -1,30 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Button from "../../components/Button/Button";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const TimesheetHeader = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const { pathname } = useLocation();
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
+    <div className="flex justify-between items-center">
       <div>
         <h1 className="text-3xl font-bold text-gray-900">
-          <Link to="/timesheets/dashboard">
-            {
-              window.location.pathname === "/timesheets/dashboard"
-                ? "Dashboard"
-                : "Timesheets"
-            }
+          <Link to="/timesheet/dashboard">
+            {pathname === "/timesheet/dashboard" ? "Dashboard" : "Timesheets"}
           </Link>
         </h1>
         <p className="text-gray-600">
@@ -33,19 +20,24 @@ const TimesheetHeader = () => {
       </div>
 
       <div className="flex gap-4">
-        {window.location.pathname === "/timesheets/dashboard" && (
-            <Button variant="primary" size="medium" onClick={navigate("/timesheets")}>
-              My Timesheets
-            </Button>)
-        }
-        {window.location.pathname === "/timesheets" && (
+        {pathname === "/timesheet/dashboard" && (
           <Button
-          variant="primary"
-          size="medium"
-          onClick={() => navigate("/timesheet/dashboard")}
-        >
-          Dashboard
-        </Button>)}
+            variant="primary"
+            size="medium"
+            onClick={() => navigate("/timesheets")}
+          >
+            My Timesheets
+          </Button>
+        )}
+        {pathname === "/timesheets" && (
+          <Button
+            variant="primary"
+            size="medium"
+            onClick={() => navigate("/timesheet/dashboard")}
+          >
+            Dashboard
+          </Button>
+        )}
         <Button
           variant="secondary"
           size="medium"
@@ -59,4 +51,3 @@ const TimesheetHeader = () => {
 };
 
 export default TimesheetHeader;
-
