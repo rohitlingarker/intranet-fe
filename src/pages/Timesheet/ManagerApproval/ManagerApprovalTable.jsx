e97fe76abbf624ec079cbf84c385b583c84648b1
Ajaybhukya <ajaykumarbhukya511@gmail.com>
2025-11-24 18:51:33 +0530

    user -> employee

--- src/pages/Timesheet/ManagerApproval/ManagerApprovalTable.jsx
+++ src/pages/Timesheet/ManagerApproval/ManagerApprovalTable.jsx
@@ -1045,7 +1045,7 @@ const ManagerApprovalTable = ({
                     <>
                       <div>
                         <label className="block text-sm font-medium text-gray-700 mb-1">
-                          Select User
+                          Select Employee
                         </label>
                         <select
                           className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
