033c427750217c4c13b7ea5c612f980dfc17a2a3
SathwikPatel1 <sathwikpatel121@gmail.com>
2026-02-27 17:34:10 +0530

    enum updated to statis

--- src/App.jsx
+++ src/App.jsx
@@ -96,8 +96,8 @@ import EmployeeDirectory from "./pages/employee-onboarding/employee-directory/Em
 import EmployeeVerification from "./pages/employee-onboarding/employee-verification/EmployeeVerification.jsx";
 import EmployeeDocumentsTemplate from "./pages/employee-onboarding/employee-documents-template/EmployeeDocumentsTemplate.jsx";
 import OrganizationTree from "./pages/employee-onboarding/organization-tree/OrganizationTree.jsx";
-import SummaryPage from  "./pages/employee-onboarding/summary-page/SummaryPage.jsx";
-import EmployeeDocumentsPage from "./pages/employee-onboarding/employeeDocuments/EmployeeDocuments.jsx";
+import SummaryPage from "./pages/employee-onboarding/summary-page/SummaryPage.jsx";
+import EmployeeDocumentsPage from "./pages/employee-onboarding/employeedocuments/EmployeeDocuments.jsx";
 import HeadcountDemographicsPage from "./pages/employee-onboarding/analytics/HeadcountDemographics.jsx";
 import EmployeeListPage from "./pages/employee-onboarding/employeelist/EmployeeList.jsx";
 import EmployeeCredentials from "./pages/employee-onboarding/employee-credentials/EmployeeCredentials.jsx";
